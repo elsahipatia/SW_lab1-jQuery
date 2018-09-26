009f9f3153782a5e210ea244b7254c28b74e6bf4
Daniel <cervera1994@gmail.com>
2018-09-26 12:17:40 +0200

    Merge branch 'master' of https://github.com/elsahipatia/SW_lab1
    # Conflicts:
    #	calculator.php

--- calculator.php
+++ calculator.php
@@ -5,30 +5,35 @@
     <title>Arithmetic Calculator</title>
 </head>
 <body>
-    <h1>Arithmetic Calculator</h1>
-    <table>
-        <tr>
-            <td align="left">Left hand side operand: </td>
-            <td><input id="leftOp" type="text"/></td>
-        </tr>
-        <tr>
-            <td align="left">Right hand side operand: </td>
-            <td><input id="rightOp" type="text"/></td>
-        </tr>
-        <tr>
-            <td align="left">Operator: </td>
-            <td><select id="op">
+<h1>Arithmetic Calculator</h1>
+<table>
+    <tr>
+        <td align="left">Left hand side operand: </td>
+        <td><input id="leftOp" type="text" oninput="calculate()"/></td>
+    </tr>
+    <tr>
+        <td align="left">Right hand side operand: </td>
+        <td><input id="rightOp" type="text" oninput="calculate()"/></td>
+    </tr>
+    <tr>
+        <td align="left">Operator: </td>
+        <td><select id="op" onchange="calculate()">
                 <option selected>+</option>
                 <option>-</option>
                 <option>*</option>
                 <option>/</option>
             </select></td>
-        </tr>
-        <tr>
-            <td><button id="cleanBtn">Clean</button></td>
-        </tr>
-    </table>
-    <div id="display"></div>
+    </tr>
+    <tr>
+        <td align="left">Result: </td>
+        <td><input id="result" type="text" oninput="calculate()"/><td/>
+    </tr>
+
+    <tr>
+        <td><button id="cleanBtn" onclick="clean()">Clean</button></td>
+    </tr>
+</table>
+<div id="display"></div>
 </body>
-<script type="text/javascript" src=""></script>
+<script type="text/javascript" src="action.js"></script>
 </html>
