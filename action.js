0c0adfcd39471bf3138ea752da097042afb9a514
Elsa S <1e.acad.uni@gmail.com>
2018-09-28 13:03:07 +0200

    improve clean funtion

--- action.js
+++ action.js
@@ -1,3 +1,9 @@
+function loadImages() {
+    document.getElementById("happyMonkey").style.display="none";
+    document.getElementById("sadMonkey").style.display="none";
+}
+
+
 
 function calculate() {
     var leftOp = document.getElementById('leftOp').value;
@@ -24,12 +30,52 @@ function compare(res) {
         return;
     }else {
         if (res == userRes) {
-            document.getElementById('display').innerText = "¡CORRECTO!";
+            var correct = document.getElementById('display')
+            correct.innerText = "CORRECT!";
+            correct.style.color ="#3dc919"
+            correct.style.fontSize="xx-large";
+            addImageCorrect();
 
         } else {
-            document.getElementById('display').innerText = "¡INCORRECTO!";
+            var wrong = document.getElementById('display')
+            wrong.innerText = "WRONG!";
+            wrong.style.color = "#F00";
+            wrong.style.fontSize="xx-large";
+            addImageIncorrect();
         }
     }
 }
 
+function clean(){
+    document.getElementById('leftOp').value='';
+    document.getElementById('rightOp').value='';
+    document.getElementById('result').value='';
+    var sadImg = document.getElementById("sadMonkey");
+    var happyImg = document.getElementById("happyMonkey");
+    var r = document.getElementById('display');
+    if (happyImg.style.display != "none")
+        happyImg.style.display = "none";
+    if (sadImg.style.display != "none"){
+        sadImg.style.display ="none";
+    }
+    r.innerText="";
 
+}
+
+function addImageCorrect(){
+    var sadImg = document.getElementById("sadMonkey");
+    var happyImg = document.getElementById("happyMonkey");
+    if (sadImg.style.display != "none"){
+        sadImg.style.display = "none";
+    }
+    happyImg.style.display = "inline";
+}
+
+function addImageIncorrect(){
+    var sadImg = document.getElementById("sadMonkey");
+    var happyImg = document.getElementById("happyMonkey");
+    if (happyImg.style.display != "none"){
+        happyImg.style.display = "none";
+    }
+    sadImg.style.display = "inline";
+}
--- calculator.php
+++ calculator.php
@@ -2,13 +2,14 @@
 <html>
 <head>
     <meta charset="utf-8">
+    <script type="text/javascript" src="action.js"></script>
     <title>Arithmetic Calculator</title>
 </head>
 <body>
 <h1>Arithmetic Calculator</h1>
+<img src="kids.png" width="300" height="300" >
 <table>
     <tr>
-        <img src="kids.png" width="300" height="300" >
         <td align="left">Left hand side operand: </td>
         <td><input id="leftOp" type="text" oninput="calculate()"/></td>
     </tr>
@@ -31,10 +32,18 @@
     </tr>
 
     <tr>
-        <td><button id="cleanBtn" onclick="document.getElementById('leftOp').value = ''; document.getElementById('rightOp').value = ''; document.getElementById('result').value = ''">Clean</button></td>
+        <td><button id="cleanBtn" onclick="clean()">Clean</button></td>
     </tr>
 </table>
 <div id="display"></div>
+<div id="imgDisplayCorrect" display="none">
+    <img src="happyMonkey.gif" id="happyMonkey">
+    <img src="sadMonkey.gif" id="sadMonkey">
+    <script language="JavaScript">
+        loadImages();
+    </script>
+</div>
+<div id="imgDisplayWrong"></div>
+
 </body>
-<script type="text/javascript" src="action.js"></script>
 </html>
